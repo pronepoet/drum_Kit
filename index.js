49057e556769e5a62ea57399ae85fa913b9ed41e
pronepoet <ochiengphillip84@gmail.com>
2022-05-06 14:10:46 +0300

    Ft: complete the app

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown',function(e){
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return;
@@ -8,11 +8,12 @@ window.addEventListener('keydown',function(e){
 
     audio.play();
     key.classList.add('playing');
-    
+
     function removeTransition(e) {
         if(e.propertyName !== 'transform') return;
         this.classList.remove('playing');
     }
 const keys = document.querySelectorAll('.key');
     keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-})
+}
+window.addEventListener('keydown',playSound)
