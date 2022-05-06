a77fd5642f081b859b63158ad83960ff9ed5bcef
pronepoet <ochiengphillip84@gmail.com>
2022-05-06 13:42:22 +0300

    Ft: added the sounds and it's now working

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 window.addEventListener('keydown',function(e){
-    console.log(e)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`)
+    if(!audio) return;
+    audio.play();
 })
