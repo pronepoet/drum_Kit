b1c5610d0c9dbc6fc0bf90fb40b8cb7ac2467a69
pronepoet <ochiengphillip84@gmail.com>
2022-05-06 13:45:25 +0300

    Ft: add rewind functionality to reset the audio playtime so that it plays when you press key repeatedly

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 window.addEventListener('keydown',function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`)
     if(!audio) return;
+    // stops when there is no keycode assigned to a prticular key
+    audio.currentTime = 0;
     audio.play();
 })
