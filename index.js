03f6773b672d4ec6b3fa1366f52a7a37b0b1e20f
pronepoet <ochiengphillip84@gmail.com>
2022-05-06 14:04:06 +0300

    Ft: add removing class feature after click

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ window.addEventListener('keydown',function(e){
     // stops when there is no keycode assigned to a prticular key
     audio.currentTime = 0;
     //rewinds time to start
+
     audio.play();
     key.classList.add('playing');
+    
+    function removeTransition(e) {
+        if(e.propertyName !== 'transform') return;
+        this.classList.remove('playing');
+    }
+const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 })
